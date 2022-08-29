322bc19015a5490500043d429a9a19018911a3a2
Chris Stolte <cstolte@scainc.com>
2022-08-29 12:22:58 -0400

    rearranged order of user selections on main screen, as per Jill M.

--- com/sca/ca/gui/MainView.py
+++ com/sca/ca/gui/MainView.py
@@ -36,47 +36,19 @@ class MainView(tk.Frame):
         self.faclist_frame = tk.Frame(self.container, height=120, pady=1, padx=5, bg='white')
         self.radius_frame = tk.Frame(self.container, height=120, pady=1, padx=5, bg='white')
 
-        self.filename_frame.grid(row=3, columnspan=5, sticky="nsew")
-        self.folder_frame.grid(row=4, columnspan=5, sticky="nsew")
-        self.faclist_frame.grid(row=5, columnspan=5, sticky="nsew")
-        self.radius_frame.grid(row=6, columnspan=5, sticky="nsew")
+        self.faclist_frame.grid(row=3, columnspan=5, sticky="nsew")
+        self.radius_frame.grid(row=4, columnspan=5, sticky="nsew")
+        self.filename_frame.grid(row=5, columnspan=5, sticky="nsew")
+        self.folder_frame.grid(row=6, columnspan=5, sticky="nsew")
 
         self.header = tk.Label(self.container, font=TEXT_FONT, bg='white', width=48,
                                text="Proximity Analysis Tool")
-        self.header.grid(row=2, column=1, sticky='WE')
+        self.header.grid(row=2, column=1, sticky='WE', pady=10)
 
-        # First step - choose a filename
-        self.step1 = tk.Label(self.filename_frame,
+        # First step - choose a facilities list file
+        self.step1 = tk.Label(self.faclist_frame,
                               text="1.", font=SMALL_TEXT_FONT, bg='white', anchor="w")
         self.step1.grid(pady=10, row=3, column=0)
-
-        self.filename_entry = EntryWithPlaceholder(
-            self.filename_frame, placeholder="Enter a filename for results", name="filename_entry")
-        self.filename_entry["width"] = 30
-        self.filename_entry.grid(row=3, column=1, pady=10)
-
-        # Second step - choose an output folder
-        self.step2 = tk.Label(self.folder_frame,
-                              text="2.", font=SMALL_TEXT_FONT, bg='white', anchor="w")
-        self.step2.grid(pady=10, row=3, column=0)
-
-        fu = PIL.Image.open('images\icons8-folder-48.png').resize((30,30))
-        ficon = self.add_margin(fu, 5, 0, 5, 0)
-        fileicon = ImageTk.PhotoImage(ficon)
-        self.fileLabel = tk.Label(self.folder_frame, image=fileicon, bg='white')
-        self.fileLabel.image = fileicon
-        self.fileLabel.grid(row=3, column=1)
-
-        self.step2_instructions = tk.Label(self.folder_frame, text="Select output folder",
-                                           font=SMALL_TEXT_FONT, bg='white', anchor="w")
-        self.step2_instructions.grid(row=3, column=2)
-        self.fileLabel.bind("<Button-1>", partial(self.browse, self.step2_instructions))
-        self.step2_instructions.bind("<Button-1>", partial(self.browse, self.step2_instructions))
-
-        # Third step - choose a facilities list file
-        self.step3 = tk.Label(self.faclist_frame,
-                              text="3.", font=SMALL_TEXT_FONT, bg='white', anchor="w")
-        self.step3.grid(pady=10, row=3, column=0)
 
         fu = PIL.Image.open('images\icons8-document-48.png').resize((30, 30))
         ficon = self.add_margin(fu, 5, 0, 5, 0)
@@ -85,25 +57,53 @@ class MainView(tk.Frame):
         self.fileLabel.image = fileicon
         self.fileLabel.grid(row=3, column=1)
 
-        self.step3_instructions = tk.Label(self.faclist_frame, text="Select facility list file",
+        self.step1_instructions = tk.Label(self.faclist_frame, text="Select facility list file",
                                            font=SMALL_TEXT_FONT, bg='white', anchor="w")
-        self.step3_instructions.grid(row=3, column=2)
-        self.fileLabel.bind("<Button-1>", partial(self.browse_file, self.step3_instructions))
-        self.step3_instructions.bind("<Button-1>", partial(self.browse_file, self.step3_instructions))
+        self.step1_instructions.grid(row=3, column=2)
+        self.fileLabel.bind("<Button-1>", partial(self.browse_file, self.step1_instructions))
+        self.step1_instructions.bind("<Button-1>", partial(self.browse_file, self.step1_instructions))
 
-        # Fourth step - choose a radius
-        self.step4 = tk.Label(self.radius_frame,
-                              text="4.", font=SMALL_TEXT_FONT, bg='white', anchor="w")
-        self.step4.grid(pady=10, row=3, column=0)
+        # Second step - choose a radius
+        self.step2 = tk.Label(self.radius_frame,
+                              text="2.", font=SMALL_TEXT_FONT, bg='white', anchor="w")
+        self.step2.grid(pady=10, row=3, column=0)
 
         self.radius_num = EntryWithPlaceholder(
             self.radius_frame, placeholder="Enter a radius ≤ 50 km", name="radius")
         self.radius_num["width"] = 24
         self.radius_num.grid(row=3, column=1, pady=10)
 
+        # Third step - choose a filename
+        self.step3 = tk.Label(self.filename_frame,
+                              text="3.", font=SMALL_TEXT_FONT, bg='white', anchor="w")
+        self.step3.grid(pady=10, row=3, column=0)
+
+        self.filename_entry = EntryWithPlaceholder(
+            self.filename_frame, placeholder="Enter a filename for results", name="filename_entry")
+        self.filename_entry["width"] = 30
+        self.filename_entry.grid(row=3, column=1, pady=10)
+
+        # Fourth step - choose an output folder
+        self.step4 = tk.Label(self.folder_frame,
+                              text="4.", font=SMALL_TEXT_FONT, bg='white', anchor="w")
+        self.step4.grid(pady=10, row=3, column=0)
+
+        fu = PIL.Image.open('images\icons8-folder-48.png').resize((30,30))
+        ficon = self.add_margin(fu, 5, 0, 5, 0)
+        fileicon = ImageTk.PhotoImage(ficon)
+        self.fileLabel = tk.Label(self.folder_frame, image=fileicon, bg='white')
+        self.fileLabel.image = fileicon
+        self.fileLabel.grid(row=3, column=1)
+
+        self.step4_instructions = tk.Label(self.folder_frame, text="Select output folder",
+                                           font=SMALL_TEXT_FONT, bg='white', anchor="w")
+        self.step4_instructions.grid(row=3, column=2)
+        self.fileLabel.bind("<Button-1>", partial(self.browse, self.step4_instructions))
+        self.step4_instructions.bind("<Button-1>", partial(self.browse, self.step4_instructions))
+
         self.run_button = tk.Label(self.container, text="Run", font=TEXT_FONT,
                                    bg='lightgrey', relief='solid', borderwidth=2, width=8)
-        self.run_button.grid(row=6, column=1, padx=20, pady=20, sticky='S')
+        self.run_button.grid(row=7, column=1, padx=20, pady=20, sticky='S')
 
         self.run_button.bind("<Button-1>", self.run_reports)
 
